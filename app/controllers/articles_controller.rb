8e6cd8da03468860770e259d1f6cc843e3652785
marcemc <marcemc@yahoo.com>
2016-02-20 09:41:16 -0500

    added controller for 'show' edit' 'index' 'update' and corresponding views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,45 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @article = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
-  def create
-    #render json: params[:article]
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to article_show(@article)
+  def edit
+    @article = Article.find(params[:id])
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Your article was created!"
+      redirect_to article_path(@article)
+    else
+
+      render 'new'
+    end
+    #render json: params[:article]
+    # @article.save
+    # redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Your Article was updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
 
   private
   def article_params
