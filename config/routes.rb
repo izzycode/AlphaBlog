3c452ff24194d694ab8b3a9f97fbc534415ca4b7
marcemc <marcemc@yahoo.com>
2016-02-23 01:52:00 -0500

    add categories with tests

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy' 
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
